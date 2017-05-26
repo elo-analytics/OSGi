6b65387c2979f7f37abc7f791317c1cf88572f63
jcosta9 <jcosta9@github.com>
2017-05-26 17:54:22 -0300

    Inserido o job de 5s

--- org.equinoxosgi.toast.dev.airbag/src/org/equinoxosgi/toast/internal/dev/airbag/fake/FakeAirbag.java
+++ org.equinoxosgi.toast.dev.airbag/src/org/equinoxosgi/toast/internal/dev/airbag/fake/FakeAirbag.java
@@ -5,11 +5,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.equinoxosgi.toast.dev.airbag.IAirbag;
 import org.equinoxosgi.toast.dev.airbag.IAirbagListener;
 
 public class FakeAirbag implements IAirbag {
-	private List listeners;
+	private List listeners = new ArrayList();
+	private Job job;
+	private boolean isRunning;
 	
 	public FakeAirbag() {
 		super();
@@ -29,5 +35,19 @@ public class FakeAirbag implements IAirbag {
 	@Override
 	public synchronized void removeListener(IAirbagListener listener){
 		listeners.remove(listener);
+	}
+	
+	/*Makes FakeAirbag deploy and notify its listeners every five seconds*/
+	public synchronized void shutdown() {
+		isRunning = true;
+		job = new Job("FakeAirbag") {
+			protected IStatus run(IProgressMonitor monitor){
+				deploy();
+				if (isRunning)
+					schedule(5000);
+				return Status.OK_STATUS;
+			}
+		};
+		job.schedule(5000);
 	}
 }
