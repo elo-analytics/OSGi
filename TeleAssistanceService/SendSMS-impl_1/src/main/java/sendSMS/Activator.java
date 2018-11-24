bad1dae42e710366380dae00ef5712f826ca49b2
Joao Paulo Costa de Araujo <jpaulo.caraujo@gmail.com>
2018-11-23 22:27:20 -0200

    Tele Assistance Service

---
+++ TeleAssistanceService/SendSMS-impl_1/src/main/java/sendSMS/Activator.java
@@ -0,0 +1,21 @@
+package sendSMS;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext arg0) throws Exception {
+		System.out.println("Starting bundle sendSMS");
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop(BundleContext arg0) throws Exception {
+		System.out.println("Stopping bundle sendSMS");
+		// TODO Auto-generated method stub
+
+	}
+
+}
